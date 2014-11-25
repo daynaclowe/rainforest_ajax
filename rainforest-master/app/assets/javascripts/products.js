dc5e0f8f860c8e9febe413d88597bb6449741deb
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-11-24 21:05:28 -0500

    AJAX refactored with shortand method $.get()

--- rainforest-master/app/assets/javascripts/products.js
+++ rainforest-master/app/assets/javascripts/products.js
@@ -1,14 +1,13 @@
 $(document).on('ready page:load', function() {
-  $('search-form').submit(function(event) {
-    event.preventDefault();
-    var searchValue = $('search').val();
+	$('search-form').submit(function(event) {
+		event.preventDefault();
+		var searchValue = $('search').val();
 
-    $.ajax({
-    	url: 'products?search=' + searchValue,
-    	type: 'GET',
-    	dataType: 'html'
-    }).done(function(data){
-    	$('#products').html(data);
-    });
-  });
+		$.get('/products?search=' + searchValue)
+		.done(function(data){
+			console.log(data);
+			$('#products').html(data);
+		});
+	});
 });
+
