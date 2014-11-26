51a59ff2d91016b723e48e9ef2f6982dce6212e3
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-11-26 16:18:50 -0500

    Added remote true to showhtmlerb

--- rainforest-master/app/controllers/products_controller.rb
+++ rainforest-master/app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    @products = if params[:search]
+    @products =  if params[:search]
       Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
     else 
-      Product.order('products.created_at DESC')
-    end.page(params[:page])
+      Product.all
+    end
+
+     @products = @products.order('products.created_at DESC').page(params[:page])
 
     respond_to do |format|
       format.html
